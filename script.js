add3bd54ac70f942e313756ffdf3ed599f04b91c
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-06-10 12:40:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  let newHexcode = shortHex.charAt(0) + shortHex.charAt(1) + shortHex.charAt(1) + shortHex.charAt(2) + shortHex.charAt(2) + shortHex.charAt(3) + shortHex.charAt(3);
+	return newHexcode;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+const shortHex = prompt("Enter Short Hex.")
 alert(extendHex(shortHex));
